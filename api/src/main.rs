d9ba88eb33c63492a4bd884645e88d8fc2f29ee2
Alexander Wilkinson <alex.wilkinson@impala.travel>
2023-11-06 20:37:30 +0000

    chore: refactored routes better under /api now can break up more as required

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,6 +1,6 @@
 use api_handlers::{
     ap_quote_handler::alan_partridge_quote_handler as ap_quote_handler,
-    octopus_handler::octopus_handler, root_handler::root_handler,
+    octopus_handler::octopus_handler,
 };
 
 use axum::{routing::get, Router};
@@ -10,16 +10,22 @@ use dotenv::dotenv;
 async fn main() {
     dotenv().ok();
 
-    let app = Router::new()
-        .route("/", get(root_handler::main))
-        .route("/alan-partridge-quote", get(ap_quote_handler::main))
-        .route("/octopus", get(octopus_handler::main))
-        .route("/octopus/about", get(octopus_handler::about))
-        .route("/octopus/gas", get(octopus_handler::get_gas_consumption))
+    let octopus_routes = Router::new()
+        .route("/", get(octopus_handler::main))
+        .route("/about", get(octopus_handler::about))
+        .route("/gas", get(octopus_handler::get_gas_consumption))
         .route(
-            "/octopus/electric",
+            "/electric",
             get(octopus_handler::get_electricity_consumption),
         );
+
+    let alan_partridge_quote_routes = Router::new().route("/", get(ap_quote_handler::main));
+
+    let api_routes = Router::new()
+        .nest("/octopus", octopus_routes)
+        .nest("/alan-partridge-quote", alan_partridge_quote_routes);
+
+    let app = Router::new().nest("/api", api_routes);
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
